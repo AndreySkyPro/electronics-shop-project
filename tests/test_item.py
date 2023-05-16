012017c33be81cd4495a56bb2dac6bb8d3fcd354
Andrey <bmw_740@mail.ru>
2023-05-16 22:38:11 +0500

    completed the task

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,20 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def testing_item():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price(testing_item):
+    """проверяем итоговую сумму при заданных параметрах товара"""
+    assert testing_item.calculate_total_price() == 200000
+
+
+def test_apply_discount(testing_item):
+    """Проверяем применение скидки с учетом изменения процента"""
+    Item.pay_rate = 0.5
+    testing_item.apply_discount()
+    assert testing_item.price == 5000
+
